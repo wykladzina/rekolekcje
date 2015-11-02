6083be3f1d3c1b618251bdb342a798084b0d8f50
piotrek <piotrek@x.xx>
2015-11-02 12:23:39 +0100

    Poprawka, żeby dzialało na PHP poniżej 5.5 (bo na serwerze mam 5.4)

--- kontrolery/Widok.php
+++ kontrolery/Widok.php
@@ -12,7 +12,9 @@ abstract class Widok {
     foreach ($this->daj_dane($serwis) as $klucz => $wartosc) {
       $smarty->assign($klucz, $wartosc);
     }
-    $krotka_nazwa_klasy = str_replace('kontrolery\\', '', static::class);
+    // Jak na serwerze będę miał PHP w wersji przynajmniej 5.5, będzie można przywrócić static::class
+    #$krotka_nazwa_klasy = str_replace('kontrolery\\', '', static::class);
+    $krotka_nazwa_klasy = str_replace('kontrolery\\', '', get_class($this));
     $smarty->assign('kontroler', $krotka_nazwa_klasy);
     $smarty->assign('najnowsze_wydarzenia', $serwis->daj_najnowsze_wydarzenia());
     $smarty->display("{$krotka_nazwa_klasy}.tpl");
